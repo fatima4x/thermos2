fdc149e905e42e89f7fa98ef42c1b6efb09a1521
Fatima <Fatima@LAPTOP-7187AP2N>
2023-05-02 15:43:37 +0530

    this is second commit

---
+++ rbca21128/src/rbca21128/Caterpillar_128.java
@@ -0,0 +1,12 @@
+package rbca21128;
+
+public class Caterpillar_128 {
+	int moves,cabbages;
+	
+	Caterpillar_128(int moves, int cabbages)
+	{
+	  this.moves=moves;
+	  this.cabbages=cabbages;
+	}
+
+}
